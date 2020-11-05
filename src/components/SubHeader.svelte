0e26af8641ebbcf0fc5710248e0ba509966cff3c
Albin Médoc <stamline203@gmail.com>
2020-11-05 17:26:02 +0100

    Created component for sub-header

---
+++ src/components/SubHeader.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let title: string;
+    export let description: string = undefined;
+</script>
+
+<style>
+    section {
+        width: calc(100% - 60px);
+        text-align: center;
+        background-color: #edb5f5;
+        color: #fff;
+        padding: 10px 30px;
+    }
+
+    section h3 {
+        margin: 0;
+        font-size: 40px;
+    }
+
+    section p {
+        margin: 0;
+        font-size: 20px;
+    }
+</style>
+
+<section>
+    <h3>{title}</h3>
+    {#if description}
+        <p>{description}</p>
+    {/if}
+    <slot />
+</section>
