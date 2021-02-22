dc4f3b80cc2137c3c586de61705936cc6ba145ce
albinmedoc <stamline203@gmail.com>
2021-02-22 22:00:35 +0100

    Fixed form width

--- src/components/OrderForm.svelte
+++ src/components/OrderForm.svelte
@@ -39,78 +39,83 @@
 </script>
 
 <style>
+    .form_container {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+    }
     .map_container {
         margin: 0 auto;
         width: 100%;
-        max-width: 600px;
         height: 400px;
     }
 </style>
-
-<Form on:submit>
-    <FormRow>
-        <InputContainer>
-            <p>
-                Godisbilen.nu är en tjänst att få godis, snacks och dryck
-                hemkört. När din beställning är lagd och bilen är framme går du
-                in i bilen och väljer själv vad du ska ha.
-            </p>
-            <h3>Komihåg!</h3>
-            <p>Minsta belopp: 100kr</p>
-            <p>Betalning sker vid leverans (endast Swish och kort)</p>
-        </InputContainer>
-    </FormRow>
-    <FormRow>
-        <InputContainer label="Telefonnummer">
-            <input
-                bind:value={phone_number}
-                type="tel"
-                placeholder=" "
-                required />
-        </InputContainer>
-    </FormRow>
-    <FormRow>
-        <InputContainer label="Adress">
-            <AutoComplete
+<div class="form_container">
+    <Form on:submit>
+        <FormRow>
+            <InputContainer>
+                <p>
+                    Godisbilen.nu är en tjänst att få godis, snacks och dryck
+                    hemkört. När din beställning är lagd och bilen är framme går du
+                    in i bilen och väljer själv vad du ska ha.
+                </p>
+                <h3>Komihåg!</h3>
+                <p>Minsta belopp: 100kr</p>
+                <p>Betalning sker vid leverans (endast Swish och kort)</p>
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer label="Telefonnummer">
+                <input
+                    bind:value={phone_number}
+                    type="tel"
+                    placeholder=" "
+                    required />
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer label="Adress">
+                <AutoComplete
+                    api_key={process.env.GOOGLE_MAPS_API_KEY}
+                    placeholder=" "
+                    required={true}
+                    bind:value={search}
+                    on:placeChanged={(event) => {
+                        updatePlace(event.detail);
+                    }} />
+            </InputContainer>
+        </FormRow>
+        {#if place}
+        <div class="map_container">
+            <Map
+                on:ready={() => {
+                    updatePlace(place);
+                }}
+                bind:map
                 api_key={process.env.GOOGLE_MAPS_API_KEY}
-                placeholder=" "
-                required={true}
-                bind:value={search}
-                on:placeChanged={(event) => {
-                    updatePlace(event.detail);
-                }} />
-        </InputContainer>
-    </FormRow>
-    {#if place}
-    <div class="map_container">
-        <Map
-            on:ready={() => {
-                updatePlace(place);
-            }}
-            bind:map
-            api_key={process.env.GOOGLE_MAPS_API_KEY}
-            options={map_options}>
-            <Marker options={{ position: place.geometry.location }}>
-                <InfoWindow
-                    options={{ content: full_address }}
-                    visible={true} />
-            </Marker>
-        </Map>
-    </div>
-{/if}
-    <FormRow>
-        <InputContainer>
-            <input type="submit" value="Lägg beställning" />
-        </InputContainer>
-    </FormRow>
-    <FormRow>
-        <InputContainer>
-            <p>
-                Genom att klicka på "Skicka" accepterar jag de
-                <a href="villkor">allmänna villkoren</a>
-                och
-                <a href="/villkor#gdpr">personuppgiftspolicyn</a>.
-            </p>
-        </InputContainer>
-    </FormRow>
-</Form>
+                options={map_options}>
+                <Marker options={{ position: place.geometry.location }}>
+                    <InfoWindow
+                        options={{ content: full_address }}
+                        visible={true} />
+                </Marker>
+            </Map>
+        </div>
+    {/if}
+        <FormRow>
+            <InputContainer>
+                <input type="submit" value="Lägg beställning" />
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer>
+                <p>
+                    Genom att klicka på "Lägg beställning" accepterar jag de
+                    <a href="villkor">allmänna villkoren</a>
+                    och
+                    <a href="/villkor#gdpr">personuppgiftspolicyn</a>.
+                </p>
+            </InputContainer>
+        </FormRow>
+    </Form>
+</div>
